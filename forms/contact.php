074dec1c6b8a5bbbeed43f0667edbd7f72ad14fd
Binal Bariya <49566176+binalbariya@users.noreply.github.com>
2022-02-13 23:16:37 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -33,13 +33,6 @@
   );
   */
 
-  $contact->smtp = array(
-    'host' => 'binalbariya@gmail.com',
-    'username' => 'binalbariya',
-    'password' => 'Binal1@gmail',
-    'port' => '587'
-  );
-
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
